f9a2e8c7c51f375851c8e1eaf5ba1f2bc39bda51
doka1101 <kangdongsoo@gmail.com>
2017-10-12 06:57:58 -0400

    update

---
+++ Machine_Learning_A_Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/nlp_scratch_pad.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 06:28:54 2017
+
+@author: dkang
+"""
+
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#importing the dataset
